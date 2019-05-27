0419c0a698cddb0bcffe2ab64109a0001b0f5b0a
Sinh <pnsinh.hg92@gmail.com>
2019-05-27 22:23:49 +0700

    Fix md5 is wrong on some device 64 bits

--- app/src/main/cpp/md5.h
+++ app/src/main/cpp/md5.h
@@ -34,7 +34,8 @@ typedef unsigned char *POINTER;
 typedef unsigned short int UINT2;
 
 /* UINT4 defines a four byte word */
-typedef unsigned long int UINT4;
+//typedef unsigned long int UINT4;
+typedef uint32_t UINT4;
 
 /* PROTO_LIST is defined depending on how PROTOTYPES is defined above.
 If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it
